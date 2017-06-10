c859017036798811310dd9c0f881f546c7653df3
Dylan Piercey <pierceydylan@gmail.com>
2017-06-09 21:34:42 -0700

    Prevent rollup warning

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import path from 'path'
 import regenerator      from 'regenerator'
 import { createFilter } from 'rollup-pluginutils'
 
+
 /**
  * rollup-plugin-regenerator
  * @param {Object} options
@@ -12,20 +13,20 @@ import { createFilter } from 'rollup-pluginutils'
  */
 function rollupPluginRegenerator(options = {}) {
   const filter = createFilter(options.include, options.exclude)
-
-  let includeRuntime = options.includeRuntime !== false
+  // Wrap runtime in another iife to prevent 'this' in root scope warning from rollup.
+  const banner = options.includeRuntime !== false
+    ? `!(function(){${fs.readFileSync(require.resolve('regenerator-runtime/runtime'), 'utf8')}}());`
+    : ''
 
   return {
     name: 'regenerator',
+    banner,
     transform: (code, id) => {
       if (!filter(id))
         return null
 
-      const result = regenerator.compile(code, { includeRuntime }).code
-      includeRuntime = false
-
       return {
-        code: result.toString(),
+        code: regenerator.compile(code).code.toString(),
         map: { mappings: '' },
       }
     },
